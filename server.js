891b647a887a2644eef4e8799c404ae24964548e
Ryan Laddusaw <rladdusaw@gmail.com>
2017-08-20 21:00:00 -0500

    Moved server.js and changed path to index.html

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.listen(port);
 
 // For all GET requests, send back index.html so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
 console.log(`Server listening on ${port}`);
