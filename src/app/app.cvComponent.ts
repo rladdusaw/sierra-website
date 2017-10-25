503ada9494ac0347ed883797f91bec8ee5044f98
Ryan Laddusaw <rladdusaw@gmail.com>
2017-10-25 18:39:29 -0500

    Created new page for CV

---
+++ src/app/app.cvComponent.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cv-component',
+    templateUrl: './app.cvComponent.html',
+    styleUrls: [ './app.cvComponent.css' ]
+})
+export class CVComponent {}
