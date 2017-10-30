e7304d6cd3233e7b7ff39a3cb0afcccd7ff81d33
Ryan Laddusaw <rladdusaw@gmail.com>
2017-10-29 20:02:07 -0500

    Fixed broken imports

--- src/app/components/app.cvComponent.ts
+++ src/app/components/app.cvComponent.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'cv-component',
-    templateUrl: './app.cvComponent.html',
-    styleUrls: [ './app.cvComponent.css' ]
+    templateUrl: '../views/app.cvComponent.html',
+    styleUrls: [ '../styles/app.cvComponent.css' ]
 })
 export class CVComponent {}
