895e30b13096f7f36672c91854127edc5697fdb8
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2017-12-24 18:18:47 -0600

    Set path for production

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const env = process.env.NODE_ENV || 'dev';
 
 // Gzip
 app.use(compression());
@@ -14,8 +15,12 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(port);
 
 // For all GET requests, send back index.html so that PathLocationStrategy can be used
+var index_path = '/src/index.html';
+if (env === 'production') {
+  index_path = '/dist/index.html';
+}
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname + index_path));
 });
 
 console.log(`Server listening on ${port}`);
