1d9cb4a82d980f0955fd3cfcf5ad8930c72e31b6
Ryan Laddusaw <rladdusaw@gmail.com>
2018-05-23 23:38:20 -0500

    Basic login working WIP

---
+++ server/server.js
@@ -0,0 +1,44 @@
+require('rootpath')();
+const compression = require('compression');
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const expressJwt = require('express-jwt');
+const config = require('config.json');
+const port = process.env.PORT || 8080;
+const env = process.env.NODE_ENV || 'dev';
+
+// Gzip
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(expressJwt({
+  secret: config.secret,
+  getToken: function(req) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+      return req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+      return req.query.token;
+    }
+    return null;
+  }
+}))
+
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default Heroku port
+app.listen(port);
+
+app.use('/users', require('./controllers/users.controller'));
+// For all GET requests, send back index.html so that PathLocationStrategy can be used
+var index_path = '/src/index.html';
+if (env === 'production') {
+  index_path = '/dist/index.html';
+}
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + index_path));
+// })
+
+console.log(`Server listening on ${port}`);
---
+++ server/services/user.service.js
@@ -0,0 +1,112 @@
+var config = require('config.json');
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var Q = require('q');
+var pgp = require('pg-promise')(/*options*/);
+var db = pgp(config.connectionString);
+
+var service = {};
+
+service.authenticate = authenticate;
+service.getAll = getAll;
+service.getById = getById;
+service.create = create;
+service.update = update;
+service.delete = _delete;
+
+module.exports = service;
+
+function authenticate(username, password) {
+  var deferred = Q.defer();
+
+  db.one('SELECT * FROM users WHERE username = $1', username)
+    .then(function(user) {
+      deferred.resolve({
+        _id: user.id,
+        username: user.username,
+        firstName: user.first_name,
+        lastName: user.last_name,
+        admin: user.admin,
+        token: jwt.sign({sub: user.id }, config.secret)
+      });
+    })
+    .catch(function(err) {
+      deferred.reject(err.name + ': ' + err.message);
+    });
+  
+  return deferred.promise;
+}
+
+function getAll() {
+  var deferred = Q.defer();
+
+  db.manyOrNone('SELECT * FROM users')
+    .then(function(users) {
+      users = _.map(users, function(user) {
+        deferred.resolve(_.omit(user, 'password_hash'));
+      });
+    })
+    .catch(function(err) {
+      deferred.reject(err.name + ': ' + err.message);
+    });
+
+    return deffered.promise;
+}
+
+function getById(id) {
+  var deferred = Q.defer();
+
+  db.oneOrNone('SELECT * FROM users where id = $1', id)
+    .then(function(user) {
+      deferred.resolve(_.omit(user, 'password_hash'));
+    })
+    .catch(function(err) {
+      deferred.reject(err.name + ': ' + err.message);
+    });
+    console.log("returning: ", deferred.promise)
+    return deferred.promise;
+}
+
+function create(userParams) {
+  var deferred = Q.defer();
+
+  db.oneOrNone('SELECT * FROM users WHERE username = $1', userParams.username)
+    .then(function(user) {
+      if (user) {
+        deferred.reject('Username "' + userParams.username + '" is already taken');
+      } else {
+        createUser();
+      }
+    })
+    .catch(function(err) {
+      deferred.reject(err.name + ': ' + err.message);
+    });
+
+    function createUser() {
+      var user = _.omit(userParams, 'password');
+      user.password_hash = bcrypt.hashSync(userParams.password, 10);
+
+      db.none('INSERT INTO users (username, password_hash, first_name, last_name) VALUES (${username}, ${password_hash}, ${first_name}, ${last_name});', {
+        username: user.username,
+        password_hash: user.password_hash,
+        first_name: user.first_name,
+        last_name: user.last_name
+      })
+      .then(function() {
+        defered.resolve();
+      })
+      .catch(function(err) {
+        deferred.reject(err.name + ': ' + err.message);
+      });
+    }
+    return deferred.promise;
+}
+
+function update() {
+
+}
+
+function _delete() {
+
+}
