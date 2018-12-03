72839738b8285382045b57931fcaa524cce8a6b7
Ryan Laddusaw <rladdusaw@gmail.com>
2018-12-02 20:51:41 -0600

    Created dummy admin component

---
+++ client/src/app/components/app.adminComponent.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'admin-component',
+    templateUrl: '../views/app.adminComponent.html',
+    styleUrls: [ '../styles/app.adminComponent.css' ]
+})
+export class AdminComponent {
+    constructor() {}
+}
